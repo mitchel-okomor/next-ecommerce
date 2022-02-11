fca8e40070e1032949093d48c140cb124c5e1208
mitchel-okomor <mitchel.okomor@gmail.com>
2022-02-11 15:11:53 +0100

    categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DataContext } from '../store/GlobalState';
 
 function Categories({ productByCategory }) {
-  const { state, dispatch } = useContext(DataContext);
+  const { state } = useContext(DataContext);
   const { categories } = state;
+  const [active, setActive] = useState('All');
+
+  const handleClick = (item) => {
+    productByCategory(item.name);
+    setActive(item.name);
+  };
 
   return (
     <div className='home-categories card m-3 p-2 border-0'>
       <h4 className='text-uppercase'>Categories</h4>
       <ul>
-        <li onClick={() => productByCategory('All')}>All</li>
+        <li
+          onClick={() => handleClick({ name: 'All' })}
+          style={{
+            backgroundColor: `${active === 'All' ? ' rgb(216, 216, 216)' : ''}`
+          }}
+        >
+          All
+        </li>
         {categories.map((item) => (
           <li
             key={item._id}
-            onClick={() => productByCategory(item.name)}
+            onClick={() => handleClick(item)}
             className='text-capitalize'
+            style={{
+              backgroundColor: `${
+                active === item.name ? 'rgb(216, 216, 216) ' : ''
+              }`
+            }}
           >
             {item.name}
           </li>
