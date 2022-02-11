e7fe8d6510fb280d48bb7b5f955d8d4617fdbb01
mitchel-okomor <mitchel.okomor@gmail.com>
2022-02-11 15:49:21 +0100

    changing base url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL: 'https://tsales.herokuapp.com/',
+    //BASE_URL: 'http://localhost:3000',
     MONGO_URL:
       'mongodb+srv://next-ecormmerce:sRKyd4dCU7sP6M6@cluster0.wu8rg.mongodb.net/next-ecormmerce?retryWrites=true&w=majority',
     ACCESS_TOKEN_SECRET: '5017since711645ladygroundjoinfish',
