84f75cf4c3a51e3e4781624c4b961fff44ae3189
mitchel-okomor <mitchel.okomor@gmail.com>
2022-02-11 15:51:45 +0100

    fixing env variable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: 'https://tsales.herokuapp.com/',
+    BASE_URL: 'https://tsales.herokuapp.com',
     //BASE_URL: 'http://localhost:3000',
     MONGO_URL:
       'mongodb+srv://next-ecormmerce:sRKyd4dCU7sP6M6@cluster0.wu8rg.mongodb.net/next-ecormmerce?retryWrites=true&w=majority',
